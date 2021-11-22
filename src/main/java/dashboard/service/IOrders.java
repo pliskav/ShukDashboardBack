d1a4701c7d2f37c096b3b9e5ddba7effa7165427
pliskav <51530522+pliskav@users.noreply.github.com>
2021-11-22 12:57:03 +0300

    Update IOrders.java

--- src/main/java/dashboard/service/IOrders.java
+++ src/main/java/dashboard/service/IOrders.java
@@ -7,7 +7,6 @@ public interface IOrders {
 	
 	public PageDTO getAllOrders(int page, int size);
 	
-	public void editOrder(Integer orderId, OrderBaseResponseDTO orderData);
 	public void editOrder(Integer orderId, OrderResponseDTO orderData);
 	
 	public void changeOrderStatus(Integer orderId, Integer orderStatus);
