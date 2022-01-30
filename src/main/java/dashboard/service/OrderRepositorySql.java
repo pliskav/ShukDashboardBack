cb568864d3cfa010fd86299b639acdee22c3dd11
pliskav <51530522+pliskav@users.noreply.github.com>
2022-01-30 10:28:03 +0300

    add total in searching with params

--- src/main/java/dashboard/service/OrderRepositorySql.java
+++ src/main/java/dashboard/service/OrderRepositorySql.java
@@ -19,6 +19,30 @@ public interface OrderRepositorySql extends CrudRepository<Orders, Integer> {
 	List<Orders> findAll();
 	
 	Orders findById(int id);
+	
+	@Query(value = "select count(distinct o.id) as total_count "
+			 
+			+ "FROM Orders o "
+			+ "inner join Users u on o.user_id=u.id "
+			+ "inner join OrderItem items on o.id=items.orderId "
+			+ "where (:email is null or u.email=:email) "
+			+ "and (:phone is null or u.phone = :phone) "
+			+ "and (:username is null or u.userName = :username) "
+			+ "and (:date is null or o.orderDate like :date) "
+			+ "and (:restaurantId is null or o.restaurant_id = :restaurantId) "
+			+ "and (:dateFrom is null or date(json_extract(o.orderDate, '$.date')) between date(:dateFrom) and date(:dateTo))"
+//			+ "group by o.id "
+			+ "having lower(group_concat(items.itemId, ',', items.quantity, ',', items.price, ',', items.itemName, ';')) like lower(:orderGoods) "
+			+ "order by o.created_at desc",
+			nativeQuery = false)
+	Long findCountofAllOrdersJoinUsersWithAllFilters(@Param(value = "email") String email, 
+																@Param("phone") String phone,
+																@Param("username") String userName,
+																@Param("date") String oredrDate,
+																@Param("restaurantId") String restaurantId,
+//																@Param("orderGoods") String goods,
+																@Param("dateFrom") LocalDate from,
+																@Param("dateTo") LocalDate to);
 
 	
 	@Query(value = "SELECT new dashboard.dto.OrderBaseResponseDTO(o.id, o.unique_order_id, o.orderstatus_id, o.user_id, o.coupon_name, o.address, o.tax, "
